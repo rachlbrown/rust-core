4195289ba738fef90d72f2cbfed3baaab2613ff7
Daniel Micay <danielmicay@gmail.com>
2013-10-20 16:49:05 -0400

    create initial numeric module structure

--- core/u32.rs
+++ core/u32.rs
@@ -7,37 +7,3 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-
-#[allow(ctypes, cstack)];
-
-pub mod clone;
-pub mod fail;
-pub mod intrinsics;
-pub mod kinds;
-pub mod ops;
-pub mod option;
-pub mod ptr;
-pub mod rc;
-pub mod slice;
-
-pub mod uint;
-pub mod u8;
-pub mod u16;
-pub mod u32;
-pub mod u64;
-
-pub mod int;
-pub mod i8;
-pub mod i16;
-pub mod i32;
-pub mod i64;
-
-pub mod f32;
-pub mod f64;
-
-#[cfg(libc)]
-pub mod heap;
-#[cfg(libc)]
-pub mod libc;
-#[cfg(libc)]
-pub mod vec;
