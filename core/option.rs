ae211f497aa2721f174231d1afe3b2a1ec4d5480
Daniel Micay <danielmicay@gmail.com>
2013-10-21 02:59:25 -0400

    add some basic `Option` methods

--- core/option.rs
+++ core/option.rs
@@ -8,7 +8,36 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use super::fail::abort;
+
 pub enum Option<T> {
     Some(T),
     None
+}
+
+impl<T> Option<T> {
+    /// Convert from `Option<T>` to `Option<&T>`
+    pub fn as_ref<'r>(&'r self) -> Option<&'r T> {
+        match *self { Some(ref x) => Some(x), None => None }
+    }
+
+    /// Convert from `Option<T>` to `Option<&mut T>`
+    pub fn as_mut<'r>(&'r mut self) -> Option<&'r mut T> {
+        match *self { Some(ref mut x) => Some(x), None => None }
+    }
+
+    /// Return the value in an `Option` or call `abort` if it is `None`.
+    pub fn get(self) -> T {
+        match self { Some(x) => x, None => abort() }
+    }
+
+    /// Maps an `Option<T>` to `Option<U>` by applying a function to a contained value.
+    pub fn map<U>(self, f: &fn(T) -> U) -> Option<U> {
+        match self { Some(x) => Some(f(x)), None => None }
+    }
+
+    /// Applies a function to the contained value or returns a default.
+    pub fn map_or<U>(self, def: U, f: &fn(T) -> U) -> U {
+        match self { None => def, Some(t) => f(t) }
+    }
 }
