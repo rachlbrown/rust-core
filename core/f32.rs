1ccbc0a19ef06038f733a70703b9cf897912c56b
Daniel Micay <danielmicay@gmail.com>
2013-10-21 02:04:46 -0400

    wrap floating point intrinsics as safe functions

--- core/f32.rs
+++ core/f32.rs
@@ -8,20 +8,82 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern "rust-intrinsic" {
-    pub fn sqrtf32(x: f32) -> f32;
-    pub fn powif32(a: f32, x: i32) -> f32;
-    pub fn sinf32(x: f32) -> f32;
-    pub fn cosf32(x: f32) -> f32;
-    pub fn powf32(a: f32, x: f32) -> f32;
-    pub fn expf32(x: f32) -> f32;
-    pub fn exp2f32(x: f32) -> f32;
-    pub fn logf32(x: f32) -> f32;
-    pub fn log10f32(x: f32) -> f32;
-    pub fn log2f32(x: f32) -> f32;
-    pub fn fmaf32(a: f32, b: f32, c: f32) -> f32;
-    pub fn fabsf32(x: f32) -> f32;
-    pub fn floorf32(x: f32) -> f32;
-    pub fn ceilf32(x: f32) -> f32;
-    pub fn truncf32(x: f32) -> f32;
+mod detail {
+    extern "rust-intrinsic" {
+        pub fn sqrtf32(x: f32) -> f32;
+        pub fn powif32(a: f32, x: i32) -> f32;
+        pub fn sinf32(x: f32) -> f32;
+        pub fn cosf32(x: f32) -> f32;
+        pub fn powf32(a: f32, x: f32) -> f32;
+        pub fn expf32(x: f32) -> f32;
+        pub fn exp2f32(x: f32) -> f32;
+        pub fn logf32(x: f32) -> f32;
+        pub fn log10f32(x: f32) -> f32;
+        pub fn log2f32(x: f32) -> f32;
+        pub fn fmaf32(a: f32, b: f32, c: f32) -> f32;
+        pub fn fabsf32(x: f32) -> f32;
+        pub fn floorf32(x: f32) -> f32;
+        pub fn ceilf32(x: f32) -> f32;
+        pub fn truncf32(x: f32) -> f32;
+    }
+}
+
+pub fn sqrt(x: f32) -> f32 {
+    unsafe { detail::sqrtf32(x) }
+}
+
+pub fn powi(x: f32, i: i32) -> f32 {
+    unsafe { detail::powif32(x, i) }
+}
+
+pub fn sin(x: f32) -> f32 {
+    unsafe { detail::sinf32(x) }
+}
+
+pub fn cos(x: f32) -> f32 {
+    unsafe { detail::cosf32(x) }
+}
+
+pub fn pow(x: f32, y: f32) -> f32 {
+    unsafe { detail::powf32(x, y) }
+}
+
+pub fn exp(x: f32) -> f32 {
+    unsafe { detail::expf32(x) }
+}
+
+pub fn exp2(x: f32) -> f32 {
+    unsafe { detail::exp2f32(x) }
+}
+
+pub fn log(x: f32) -> f32 {
+    unsafe { detail::logf32(x) }
+}
+
+pub fn log10(x: f32) -> f32 {
+    unsafe { detail::log10f32(x) }
+}
+
+pub fn log2(x: f32) -> f32 {
+    unsafe { detail::log2f32(x) }
+}
+
+pub fn fma(a: f32, b: f32, c: f32) -> f32 {
+    unsafe { detail::fmaf32(a, b, c) }
+}
+
+pub fn abs(x: f32) -> f32 {
+    unsafe { detail::fabsf32(x) }
+}
+
+pub fn floor(x: f32) -> f32 {
+    unsafe { detail::floorf32(x) }
+}
+
+pub fn ceil(x: f32) -> f32 {
+    unsafe { detail::ceilf32(x) }
+}
+
+pub fn trunc(x: f32) -> f32 {
+    unsafe { detail::truncf32(x) }
 }
--- core/f64.rs
+++ core/f64.rs
@@ -8,20 +8,82 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern "rust-intrinsic" {
-    pub fn sqrtf64(x: f64) -> f64;
-    pub fn powif64(a: f64, x: i32) -> f64;
-    pub fn sinf64(x: f64) -> f64;
-    pub fn cosf64(x: f64) -> f64;
-    pub fn powf64(a: f64, x: f64) -> f64;
-    pub fn expf64(x: f64) -> f64;
-    pub fn exp2f64(x: f64) -> f64;
-    pub fn logf64(x: f64) -> f64;
-    pub fn log10f64(x: f64) -> f64;
-    pub fn log2f64(x: f64) -> f64;
-    pub fn fmaf64(a: f64, b: f64, c: f64) -> f64;
-    pub fn fabsf64(x: f64) -> f64;
-    pub fn floorf64(x: f64) -> f64;
-    pub fn ceilf64(x: f64) -> f64;
-    pub fn truncf64(x: f64) -> f64;
+mod detail {
+    extern "rust-intrinsic" {
+        pub fn sqrtf64(x: f64) -> f64;
+        pub fn powif64(a: f64, x: i32) -> f64;
+        pub fn sinf64(x: f64) -> f64;
+        pub fn cosf64(x: f64) -> f64;
+        pub fn powf64(a: f64, x: f64) -> f64;
+        pub fn expf64(x: f64) -> f64;
+        pub fn exp2f64(x: f64) -> f64;
+        pub fn logf64(x: f64) -> f64;
+        pub fn log10f64(x: f64) -> f64;
+        pub fn log2f64(x: f64) -> f64;
+        pub fn fmaf64(a: f64, b: f64, c: f64) -> f64;
+        pub fn fabsf64(x: f64) -> f64;
+        pub fn floorf64(x: f64) -> f64;
+        pub fn ceilf64(x: f64) -> f64;
+        pub fn truncf64(x: f64) -> f64;
+    }
+}
+
+pub fn sqrt(x: f64) -> f64 {
+    unsafe { detail::sqrtf64(x) }
+}
+
+pub fn powi(x: f64, i: i32) -> f64 {
+    unsafe { detail::powif64(x, i) }
+}
+
+pub fn sin(x: f64) -> f64 {
+    unsafe { detail::sinf64(x) }
+}
+
+pub fn cos(x: f64) -> f64 {
+    unsafe { detail::cosf64(x) }
+}
+
+pub fn pow(x: f64, y: f64) -> f64 {
+    unsafe { detail::powf64(x, y) }
+}
+
+pub fn exp(x: f64) -> f64 {
+    unsafe { detail::expf64(x) }
+}
+
+pub fn exp2(x: f64) -> f64 {
+    unsafe { detail::exp2f64(x) }
+}
+
+pub fn log(x: f64) -> f64 {
+    unsafe { detail::logf64(x) }
+}
+
+pub fn log10(x: f64) -> f64 {
+    unsafe { detail::log10f64(x) }
+}
+
+pub fn log2(x: f64) -> f64 {
+    unsafe { detail::log2f64(x) }
+}
+
+pub fn fma(a: f64, b: f64, c: f64) -> f64 {
+    unsafe { detail::fmaf64(a, b, c) }
+}
+
+pub fn abs(x: f64) -> f64 {
+    unsafe { detail::fabsf64(x) }
+}
+
+pub fn floor(x: f64) -> f64 {
+    unsafe { detail::floorf64(x) }
+}
+
+pub fn ceil(x: f64) -> f64 {
+    unsafe { detail::ceilf64(x) }
+}
+
+pub fn trunc(x: f64) -> f64 {
+    unsafe { detail::truncf64(x) }
 }
