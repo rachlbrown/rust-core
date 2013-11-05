c98204332fc502ea5030fe3a266b10c0a5b5360d
Daniel Micay <danielmicay@gmail.com>
2013-11-04 21:28:40 -0500

    vec: start using the Allocator trait
    A finished implementation of this is blocked due to generic destructors
    not working.

--- core/vec.rs
+++ core/vec.rs
@@ -8,35 +8,56 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use super::mem::{move_val_init, size_of, transmute};
-use super::heap::{out_of_memory, malloc_raw, realloc_raw, free};
+use super::mem::{Allocator, move_val_init, size_of, transmute};
+use super::heap::{Heap, out_of_memory, malloc_raw, free};
 use super::kinds::{Freeze, Send};
 use super::ops::Drop;
 use super::slice::Slice;
 use super::ptr::{offset, read_ptr};
 use super::uint::mul_with_overflow;
 
-pub struct Vec<T> {
+pub struct Vec<T, A> {
     priv len: uint,
     priv cap: uint,
-    priv ptr: *mut T
+    priv ptr: *mut T,
+    priv alloc: A
 }
 
-impl<T: Send + Freeze> Vec<T> {
-    #[inline]
-    pub fn new() -> Vec<T> {
-        Vec { len: 0, cap: 0, ptr: 0 as *mut T }
+impl<T: Send + Freeze> Vec<T, Heap> {
+    #[inline(always)]
+    pub fn new() -> Vec<T, Heap> {
+        Vec { len: 0, cap: 0, ptr: 0 as *mut T, alloc: Heap }
     }
 
-    pub fn with_capacity(capacity: uint) -> Vec<T> {
+    pub fn with_capacity(capacity: uint) -> Vec<T, Heap> {
         let (size, overflow) = mul_with_overflow(capacity, size_of::<T>());
         if overflow {
             out_of_memory();
         }
         let ptr = unsafe { malloc_raw(size) };
-        Vec { len: 0, cap: capacity, ptr: ptr as *mut T }
+        Vec { len: 0, cap: capacity, ptr: ptr as *mut T, alloc: Heap }
+    }
+}
+
+/* FIXME: blocked on the destructor being able to use the allocator (see below)
+impl<T: Send + Freeze, A: Allocator> Vec<T, A> {
+    #[inline(always)]
+    pub fn with_alloc(alloc: A) -> Vec<T, A> {
+        Vec { len: 0, cap: 0, ptr: 0 as *mut T, alloc: alloc }
     }
 
+    pub fn with_alloc_capacity(mut alloc: A, capacity: uint) -> Vec<T, A> {
+        let (size, overflow) = mul_with_overflow(capacity, size_of::<T>());
+        if overflow {
+            out_of_memory();
+        }
+        let (ptr, _) = unsafe { alloc.alloc(size) };
+        Vec { len: 0, cap: capacity, ptr: ptr as *mut T, alloc: alloc }
+    }
+}
+*/
+
+impl<T: Send + Freeze, A: Allocator> Vec<T, A> {
     #[inline(always)]
     pub fn len(&self) -> uint {
         self.len
@@ -51,12 +72,12 @@ impl<T: Send + Freeze> Vec<T> {
     pub fn shrink_to_fit(&mut self) {
         unsafe {
             if self.len == 0 {
-                free(self.ptr as *mut u8);
+                self.alloc.free(self.ptr as *mut u8);
                 self.cap = 0;
                 self.ptr = 0 as *mut T;
             } else {
-                self.cap = self.len;
-                self.ptr = realloc_raw(self.ptr as *mut u8, self.cap * size_of::<T>()) as *mut T;
+                let (ptr, _) = self.alloc.realloc(self.ptr as *mut u8, self.cap * size_of::<T>());
+                self.ptr = ptr as *mut T;
             }
         }
     }
@@ -70,7 +91,8 @@ impl<T: Send + Freeze> Vec<T> {
                 self.cap = self.cap * 2;
                 let size = old_size * 2;
                 if old_size > size { out_of_memory() }
-                self.ptr = realloc_raw(self.ptr as *mut u8, size) as *mut T;
+                let (ptr, _) = self.alloc.realloc(self.ptr as *mut u8, size);
+                self.ptr = ptr as *mut T;
             }
 
             let end = offset(self.ptr as *T, self.len as int) as *mut T;
@@ -93,8 +115,9 @@ impl<T: Send + Freeze> Vec<T> {
 }
 
 
+// FIXME: use the allocator, blocked on https://github.com/mozilla/rust/issues/4252
 #[unsafe_destructor]
-impl<T: Send + Freeze> Drop for Vec<T> {
+impl<T: Send + Freeze> Drop for Vec<T, Heap> {
     fn drop(&mut self) {
         unsafe {
             let mut i = 0;
