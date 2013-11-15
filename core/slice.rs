b917f9b4aee2a9e2806131b242429cf6641de50d
Daniel Micay <danielmicay@gmail.com>
2013-11-14 22:14:00 -0500

    add basic slice functions

--- core/slice.rs
+++ core/slice.rs
@@ -8,7 +8,82 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use core::mem::transmute;
+use core::ptr::{offset, read_ptr, swap_ptr};
+use core::fail::abort;
+
 pub struct Slice<T> {
     data: *T,
     len: uint
+}
+
+pub fn len<T>(xs: &[T]) -> uint {
+    unsafe {
+        let slice: Slice<T> = transmute(xs);
+        slice.len
+    }
+}
+
+pub fn slice<'a, T>(xs: &'a [T], start: uint, end: uint) -> &'a [T] {
+    if start > end || end > len(xs) {
+        abort()
+    }
+    unsafe {
+        let slice: Slice<T> = transmute(xs);
+        let new = Slice {
+            data: offset(slice.data, start as int),
+            len: (end - start)
+        };
+        transmute(new)
+    }
+}
+
+pub fn slice_from<'a, T>(xs: &'a [T], start: uint) -> &'a [T] {
+    slice(xs, start, len(xs))
+}
+
+pub fn slice_to<'a, T>(xs: &'a [T], end: uint) -> &'a [T] {
+    slice(xs, 0, end)
+}
+
+pub fn split<'a, T>(xs: &'a [T], mid: uint) -> (&'a [T], &'a [T]) {
+    (slice_to(xs, mid), slice_from(xs, mid))
+}
+
+pub fn mut_slice<'a, T>(xs: &'a mut [T], start: uint, end: uint) -> &'a mut [T] {
+    if start > end || end > len(xs) {
+        abort()
+    }
+    unsafe {
+        let slice: Slice<T> = transmute(xs);
+        let new = Slice {
+            data: offset(slice.data, start as int),
+            len: (end - start)
+        };
+        transmute(new)
+    }
+}
+
+pub fn mut_slice_from<'a, T>(xs: &'a mut [T], start: uint) -> &'a mut [T] {
+    let length = len(xs);
+    mut_slice(xs, start, length)
+}
+
+pub fn mut_slice_to<'a, T>(xs: &'a mut [T], end: uint) -> &'a mut [T] {
+    mut_slice(xs, 0, end)
+}
+
+pub fn mut_split<'a, T>(xs: &'a mut [T], mid: uint) -> (&'a mut [T], &'a mut [T]) {
+    unsafe {
+        let ys: &'a mut [T] = read_ptr(&xs);
+        (mut_slice_to(xs, mid), mut_slice_from(ys, mid))
+    }
+}
+
+pub fn swap<T>(xs: &mut [T], a: uint, b: uint) {
+    unsafe {
+        let x: *mut T = &mut xs[a];
+        let y: *mut T = &mut xs[b];
+        swap_ptr(x, y);
+    }
 }
