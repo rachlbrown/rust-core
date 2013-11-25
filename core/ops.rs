f2c8997641c0e7063859093da686c7d969a132fc
Daniel Micay <danielmicay@gmail.com>
2013-11-25 14:52:14 -0500

    add implementations of `Eq` and `Ord`

--- core/ops.rs
+++ core/ops.rs
@@ -87,6 +87,110 @@ pub trait Eq {
     fn ne(&self, other: &Self) -> bool { !self.eq(other) }
 }
 
+impl Eq for int {
+    #[inline(always)]
+    fn eq(&self, other: &int) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &int) -> bool { *self != *other }
+}
+
+impl Eq for i8 {
+    #[inline(always)]
+    fn eq(&self, other: &i8) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &i8) -> bool { *self != *other }
+}
+
+impl Eq for i16 {
+    #[inline(always)]
+    fn eq(&self, other: &i16) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &i16) -> bool { *self != *other }
+}
+
+impl Eq for i32 {
+    #[inline(always)]
+    fn eq(&self, other: &i32) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &i32) -> bool { *self != *other }
+}
+
+impl Eq for i64 {
+    #[inline(always)]
+    fn eq(&self, other: &i64) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &i64) -> bool { *self != *other }
+}
+
+impl Eq for uint {
+    #[inline(always)]
+    fn eq(&self, other: &uint) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &uint) -> bool { *self != *other }
+}
+
+impl Eq for u8 {
+    #[inline(always)]
+    fn eq(&self, other: &u8) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &u8) -> bool { *self != *other }
+}
+
+impl Eq for u16 {
+    #[inline(always)]
+    fn eq(&self, other: &u16) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &u16) -> bool { *self != *other }
+}
+
+impl Eq for u32 {
+    #[inline(always)]
+    fn eq(&self, other: &u32) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &u32) -> bool { *self != *other }
+}
+
+impl Eq for u64 {
+    #[inline(always)]
+    fn eq(&self, other: &u64) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &u64) -> bool { *self != *other }
+}
+
+impl Eq for () {
+    #[inline(always)]
+    fn eq(&self, _: &()) -> bool { true }
+
+    #[inline(always)]
+    fn ne(&self, _: &()) -> bool { true }
+}
+
+impl Eq for bool {
+    #[inline(always)]
+    fn eq(&self, other: &bool) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &bool) -> bool { *self != *other }
+}
+
+impl Eq for char {
+    #[inline(always)]
+    fn eq(&self, other: &char) -> bool { *self == *other }
+
+    #[inline(always)]
+    fn ne(&self, other: &char) -> bool { *self != *other }
+}
+
 #[lang="ord"]
 pub trait Ord {
     #[inline(always)]
@@ -114,6 +218,174 @@ impl Ord for int {
 
     #[inline(always)]
     fn ge(&self, other: &int) -> bool { *self >= *other }
+}
+
+impl Ord for i8 {
+    #[inline(always)]
+    fn lt(&self, other: &i8) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &i8) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &i8) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &i8) -> bool { *self >= *other }
+}
+
+impl Ord for i16 {
+    #[inline(always)]
+    fn lt(&self, other: &i16) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &i16) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &i16) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &i16) -> bool { *self >= *other }
+}
+
+impl Ord for i32 {
+    #[inline(always)]
+    fn lt(&self, other: &i32) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &i32) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &i32) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &i32) -> bool { *self >= *other }
+}
+
+impl Ord for i64 {
+    #[inline(always)]
+    fn lt(&self, other: &i64) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &i64) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &i64) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &i64) -> bool { *self >= *other }
+}
+
+impl Ord for uint {
+    #[inline(always)]
+    fn lt(&self, other: &uint) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &uint) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &uint) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &uint) -> bool { *self >= *other }
+}
+
+impl Ord for u8 {
+    #[inline(always)]
+    fn lt(&self, other: &u8) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &u8) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &u8) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &u8) -> bool { *self >= *other }
+}
+
+impl Ord for u16 {
+    #[inline(always)]
+    fn lt(&self, other: &u16) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &u16) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &u16) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &u16) -> bool { *self >= *other }
+}
+
+impl Ord for u32 {
+    #[inline(always)]
+    fn lt(&self, other: &u32) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &u32) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &u32) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &u32) -> bool { *self >= *other }
+}
+
+impl Ord for u64 {
+    #[inline(always)]
+    fn lt(&self, other: &u64) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &u64) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &u64) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &u64) -> bool { *self >= *other }
+}
+
+impl Ord for () {
+    #[inline(always)]
+    fn lt(&self, _: &()) -> bool { false }
+
+    #[inline(always)]
+    fn le(&self, _: &()) -> bool { true }
+
+    #[inline(always)]
+    fn gt(&self, _: &()) -> bool { false }
+
+    #[inline(always)]
+    fn ge(&self, _: &()) -> bool { true }
+}
+
+impl Ord for bool {
+    #[inline(always)]
+    fn lt(&self, other: &bool) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &bool) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &bool) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &bool) -> bool { *self >= *other }
+}
+
+impl Ord for char {
+    #[inline(always)]
+    fn lt(&self, other: &char) -> bool { *self < *other }
+
+    #[inline(always)]
+    fn le(&self, other: &char) -> bool { *self <= *other }
+
+    #[inline(always)]
+    fn gt(&self, other: &char) -> bool { *self > *other }
+
+    #[inline(always)]
+    fn ge(&self, other: &char) -> bool { *self >= *other }
 }
 
 impl<T: Ord> Ord for ~T {
